357097b9314842be65d3ba3ea99f53c149f0e5e2
Ryan Zogg <rzogg@ault.cscs.ch>
2025-03-13 18:13:43 +0100

    Add script to create data subsets

--- prepare_input.sh
+++ prepare_input.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 workdir=`pwd`
-dataset= 20130502			# valid: 20130502 or 20110521
+dataset=20110521 			# valid: 20130502 or 20110521
 
 cd $workdir/data/$dataset
 for i in {1..22}
@@ -9,11 +9,19 @@ do
   gunzip -k ALL.chr${i}.250000.vcf.gz
 done
 
+if [[ "$dataset" == "20110521" ]]; then
+	base_url="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/$dataset/"
+	file_name=".phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
+else 
+	base_url="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/$dataset/supporting/functional_annotation/filtered/"
+	 file_name=".phase3_shapeit2_mvncall_integrated_v5.$dataset.sites.annotation.vcf.gz"
+fi
+
 cd $workdir/data/$dataset/sifting
 for i in {1..22}
 do
-  wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/$dataset/supporting/functional_annotation/filtered/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5.$dataset.sites.annotation.vcf.gz
-  gunzip -k ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5.$dataset.sites.annotation.vcf.gz
+  wget -c "${base_url}ALL.chr${i}${file_name}"
+  gunzip -k "All.chr${i}$file_name"
 done
 
 cd $workdir
